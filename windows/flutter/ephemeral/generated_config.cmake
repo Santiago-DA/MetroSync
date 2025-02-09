f0255ace3ebcf6d3ebd1f936b46864a1bcec02e1
Vincenzo <120345630+Vincenzo-ML@users.noreply.github.com>
2025-02-09 13:06:30 -0400

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Enzo\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Enzo\\Desktop\\flutter_prueba" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Enzo\\flutter"
+  "PROJECT_DIR=C:\\Users\\Enzo\\Desktop\\flutter_prueba"
+  "FLUTTER_ROOT=C:\\Users\\Enzo\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Enzo\\Desktop\\flutter_prueba\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Enzo\\Desktop\\flutter_prueba"
+  "FLUTTER_TARGET=C:\\Users\\Enzo\\Desktop\\flutter_prueba\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Enzo\\Desktop\\flutter_prueba\\.dart_tool\\package_config.json"
+)
