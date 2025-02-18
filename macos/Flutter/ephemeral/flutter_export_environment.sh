ff98d5528aaa973c3bf8b010f62d0264721d954b
Gabriel <gabgaru@gmail.com>
2025-02-18 10:52:55 -0400

    Schedule and view reorganization

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Enzo\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Enzo\Desktop\flutter_prueba"
+export "FLUTTER_ROOT=C:\SDK Flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\gabga\Desktop\Software\MetroSync"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
--- windows/flutter/ephemeral/generated_config.cmake
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -1,6 +1,6 @@
 # Generated code do not commit.
-file(TO_CMAKE_PATH "C:\\Users\\Enzo\\flutter" FLUTTER_ROOT)
-file(TO_CMAKE_PATH "C:\\Users\\Enzo\\Desktop\\flutter_prueba" PROJECT_DIR)
+file(TO_CMAKE_PATH "C:\\SDK Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\gabga\\Desktop\\Software\\MetroSync" PROJECT_DIR)
 
 set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
 set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
@@ -10,14 +10,14 @@ set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
 
 # Environment variables to pass to tool_backend.sh
 list(APPEND FLUTTER_TOOL_ENVIRONMENT
-  "FLUTTER_ROOT=C:\\Users\\Enzo\\flutter"
-  "PROJECT_DIR=C:\\Users\\Enzo\\Desktop\\flutter_prueba"
-  "FLUTTER_ROOT=C:\\Users\\Enzo\\flutter"
-  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Enzo\\Desktop\\flutter_prueba\\windows\\flutter\\ephemeral"
-  "PROJECT_DIR=C:\\Users\\Enzo\\Desktop\\flutter_prueba"
-  "FLUTTER_TARGET=C:\\Users\\Enzo\\Desktop\\flutter_prueba\\lib\\main.dart"
+  "FLUTTER_ROOT=C:\\SDK Flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\gabga\\Desktop\\Software\\MetroSync"
+  "FLUTTER_ROOT=C:\\SDK Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\gabga\\Desktop\\Software\\MetroSync\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\gabga\\Desktop\\Software\\MetroSync"
+  "FLUTTER_TARGET=C:\\Users\\gabga\\Desktop\\Software\\MetroSync\\lib\\main.dart"
   "DART_OBFUSCATION=false"
   "TRACK_WIDGET_CREATION=true"
   "TREE_SHAKE_ICONS=false"
-  "PACKAGE_CONFIG=C:\\Users\\Enzo\\Desktop\\flutter_prueba\\.dart_tool\\package_config.json"
+  "PACKAGE_CONFIG=C:\\Users\\gabga\\Desktop\\Software\\MetroSync\\.dart_tool\\package_config.json"
 )
