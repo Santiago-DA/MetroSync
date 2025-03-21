e758cc038ad8d9e1ec4c7f9a816c9bb569efbea2
Santiago-DA <sa.aristimuno@correo.unimet.edu.ve>
2025-03-21 13:25:11 -0400

    from id to _id

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,22 @@ import 'MongoManager/MongoDB.dart';
 import 'GUI/GUI.dart';
 import 'ViewModel/ViewModel.dart';
 import 'lost_item/lost_item.dart';
-int n=0;
+
+int n = 0;
 Future<void> main() async {
   //WidgetsFlutterBinding.ensureInitialized();
   await MongoDB.connect();
-  VM vm=VM();
+  VM vm = VM();
   // vm.crearItem('title', 'tag', 'imageURL');
   // vm.crearItem('title2', 'tag', 'imageURL');
   // vm.crearItem('title3', 'tag', 'imageURL');
   // vm.crearItem('title4', 'tag', 'imageURL');
   // vm.crearItem('title5', 'tag', 'imageURL');
-  n=n+1;
+  n = n + 1;
   print('Main ejecutado $n veces');
   await vm.loaditemfromBD();
-  await vm.eliminarItem(vm.itemxnombre('title2').id);
-  await vm.reclamarItem(vm.itemxnombre('title3').id);
+  //await vm.eliminarItem(vm.itemxnombre('title2').id);
+  bool b = await vm.reclamarItem(vm.itemxnombre('title3').id);
   print(vm.getlostitems());
   //runApp(MyApp());
 }
